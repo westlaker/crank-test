8427585dafa40d559e49539919ddb5532d4b1adb
Jici Gao <westlakers@gmail.com>
2024-11-14 18:09:59 +0000

    modify runorchard.sh

--- runorchard.sh
+++ runorchard.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Define an array with the custom step values
-steps=(1 2 3 4 8 12 15 16)
+#steps=(1 2 3 4 8 12 15 16)
+first=26
+steps=(${first} 27 28 29 33 37 40 41)
 
 # Loop through each value in the array
 for step in "${steps[@]}"; do
-    echo "Processing cores: 1..$step"
+    echo "Processing cores: ${first}..$step"
 	
-    sudo sh orchardj.sh 1 $step
+    sudo  ./orchardj.sh ${first} $step
 done
 
 echo "completed!"
