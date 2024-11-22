3dba7ce0ff6a5b3351e899c712cc46adf975a6b9
Ubuntu <ubuntu@1pbeaverton-534.test.ast.arm.com>
2024-11-22 16:36:34 -0600

    fix runorchard.sh

--- runorchard.sh
+++ runorchard.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Define an array with the custom step values
-#steps=(1 2 3 4 8 12 15 16)
-first=26
-steps=(${first} 27 28 29 33 37 40 41)
+#first=26
+#steps=(${first} 27 28 29 33 37 40 41)
+steps=(1 2 4 8 16)
+first=1
 
 # Loop through each value in the array
 for step in "${steps[@]}"; do
     echo "Processing cores: ${first}..$step"
 	
-    sudo  ./orchardj.sh ${first} $step
+    sudo  ./orchardj.sh $first $step
 done
 
 echo "completed!"
