72f17a3e8be12cb1a45d03572b0a82b639b4dfff
Ubuntu <ubuntu@1pbeaverton-534.test.ast.arm.com>
2024-11-25 11:14:03 -0600

    add perf stat

--- runtopdownsignal.sh
+++ runtopdownsignal.sh
@@ -12,6 +12,9 @@ handle_signal() {
     echo "<<< Signal received! Starting topdown on cores: $val1 .. $val2"
     sleep 5  # Simulate Program 2 running
     ./telemetry-solution/tools/topdown_tool/topdown-tool -C $val1-$val2 sleep 10 > topdown_warmup120s_$val1-$val2-1.txt
+    sleep 6
+    sudo perf stat -e r148,r149,r150,r36,r37 -p ${pid} sleep 8 > perfstat_warmup120s_$val1-$val2.txt 2>&1
+    sudo perf stat -e r002,r005,r02D,r36,r37 -p ${pid} sleep 8 >> perfstat_warmup120s_$val1-$val2.txt 2>&1
     echo ">>> topdown completed with parameter: $val1 and $val2"
     signal_received=1  # Set the flag to exit the loop
 }
